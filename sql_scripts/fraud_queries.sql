c409f813c7d5287022f1e57517abbc42ecb8dbcb
Rohit Pawar <161500406+Rohit7696@users.noreply.github.com>
2026-04-01 00:46:06 +0530

    Add files via upload

---
+++ sql_scripts/fraud_queries.sql
@@ -0,0 +1,16 @@
+-- Create the Environment
+CREATE DATABASE IF NOT EXISTS FinanceRiskDB;
+USE FinanceRiskDB;
+
+-- Define the Table Structure
+CREATE TABLE transactions (
+    distance_from_home FLOAT,
+    distance_from_last_transaction FLOAT,
+    ratio_to_median_purchase_price FLOAT,
+    repeat_retailer INT, -- 1 = Yes, 0 = No
+    used_chip INT,
+    used_pin_number INT,
+    online_order INT,
+    fraud INT -- Our Target Column
+);
+
